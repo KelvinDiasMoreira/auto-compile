a58079b32f0a423d8b99be20e85d1156ca461020
Kelvi <kelvingremista2015@hotmail.com>
2024-07-16 23:12:40 -0300

    construction

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// int strcmp_s(char *first_string, char second_string)
+// {
+//     printf(first_string);
+//     printf(second_string);
+//     return 1;
+// }
+
 void set_path(const char *str, int *ptr)
 {
-    // printf("pointer: %d\n", ptr);
-    printf("%s\n", str);
+    char path[6];
+    path[0] = "-";
+    path[1] = "-";
+    path[2] = "p";
+    path[3] = "a";
+    path[4] = "t";
+    path[5] = "h";
+
+    printf("pointer: %s\n", path);
+    // printf("%s\n", str);
+    // if (strcmp_s(str, '--path') != 0)
+    // {
+    //     printf("equal");
+    // }
 }
 
 int main(int argc, char const *argv[])
